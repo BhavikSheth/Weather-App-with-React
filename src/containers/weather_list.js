356aca980f3cd300bf38b8e6246c48a6d1cf6c17
Bhavik Sheth <bhaviksheth20@gmail.com>
2017-11-22 15:58:18 +0000

    add css styling

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -6,16 +6,16 @@ class WeatherList extends Component {
   renderWeather(cityData) {
     console.log(cityData);
     const name = cityData.city.name;
-    const temps = cityData.list.map(weather => weather.main.temp);
+    const temps = cityData.list.map(weather => weather.main.temp-273.15);
     const pressures = cityData.list.map(weather => weather.main.pressure);
     const humidities = cityData.list.map(weather => weather.main.humidity);
 
     return (
       <tr key={name}>
         <td>{name}</td>
-        <td><Chart data={temps} color="orange" /></td>
-        <td><Chart data={pressures} color="green" /></td>
-        <td><Chart data={humidities} color="black" /></td>
+        <td><Chart data={temps} unit="&deg;C" color="orange" /></td>
+        <td><Chart data={pressures} unit="hPa" color="green" /></td>
+        <td><Chart data={humidities} unit="%" color="black" /></td>
       </tr>
     );
   }
@@ -26,9 +26,9 @@ class WeatherList extends Component {
         <thead>
           <tr>
             <th>City</th>
-            <th>Temperature</th>
-            <th>Pressure</th>
-            <th>Humidity</th>
+            <th>Temperature (&deg;C)</th>
+            <th>Pressure (hPa)</th>
+            <th>Humidity (%)</th>
           </tr>
         </thead>
         <tbody>
