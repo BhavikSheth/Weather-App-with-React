5953660d769cc7623c377c9721f4522389dbc5aa
Bhavik Sheth <bhaviksheth20@gmail.com>
2017-11-22 01:11:09 +0000

    install axios and build function to fetch weather data for a given location

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_KEY = '4d559009ea1994fdbcb6d14e1cab3fe6';
+const ROOL_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOL_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  }
+}
